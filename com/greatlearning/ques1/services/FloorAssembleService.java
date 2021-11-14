0a7aa28f062f114a99759857f661a6c28102cacb
rnaaz <rohinanaaz@gmail.com>
2021-11-14 19:01:44 +0530

    Initial commit

--- com/greatlearning/ques1/services/FloorAssembleService.java
+++ com/greatlearning/ques1/services/FloorAssembleService.java
@@ -4,62 +4,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FloorAssembleService {
-	 
-	    int presentFloor;
-	    List<Integer> floorsOnSpecificDay;
 
-    /**
-     * To assemble the floors
-     * @param floorSizes list of input of floor sizes.
-     */
-    public void assembleFloor(List<Integer> floorSize) {
+	int currentFloor;
+	List<Integer> floorsOnSpecificDay;
 
+	// Function to assemble the floors
+	public void assembleFloor(List<Integer> floorSize) {
 
-        List<Integer> floorsLeft = new ArrayList<>();
-        int maxFloorNum = floorSize.size();
-        presentFloor = maxFloorNum;
+		List<Integer> floorsLeft = new ArrayList<>();
+		int maxFloorNum = floorSize.size();
+		currentFloor = maxFloorNum;
 
+		for (int i = 0; i < maxFloorNum; i++) {
 
-        for (int i = 0; i < maxFloorNum; i++) {
+			floorsOnSpecificDay = new ArrayList<>();
 
-            floorsOnSpecificDay = new ArrayList<>();
+			int size = floorSize.get(i);
+			
+			// check if size is same as present floor
+			
+			if (size == currentFloor) {
+				floorsOnSpecificDay.add(size);
+				currentFloor--;
+				RestOfTheFloors(currentFloor, floorsLeft);
+			}
+			
+			// check if size is less than present floor
 
-            int size = floorSize.get(i);
-            //check if size is same as present floor
-            if (size == presentFloor) {
-                floorsOnSpecificDay.add(size);
-                presentFloor--;
-                RestOfTheFloors(presentFloor, floorsLeft);
-            }
-            //check if size is less than present floor , if true add in remainingfloors
+			if (size < currentFloor) {
+				floorsLeft.add(size);
+			}
 
-            if (size < presentFloor) {
-            	floorsLeft.add(size);
-            }
+			// if no floors assembled, print empty line
+			
+			System.out.println();
+			System.out.println("Day: " + (i + 1));
+			for (int j : floorsOnSpecificDay) {
+				System.out.print(j + " ");
+			}
+		}
+	}
 
-
-
-            //if no floors assembled print empty line
-            System.out.println();
-            System.out.println("Day: " + (i + 1));
-            for (int j : floorsOnSpecificDay) {
-                System.out.print(j + " ");
-            }
-        }
-    }
-
-    /**
-     * to check the list of remaining floors to be added to the stack on nth day
-     * @param currentFloor pointer to the current floor
-     * @param remainingList remaining floors
-     */
-    private void RestOfTheFloors(int currentFloor, List<Integer> remainingList) {
-        for (int i : remainingList) {
-            if (i == currentFloor) {
-                floorsOnSpecificDay.add(i);
-                presentFloor = currentFloor - 1;
-                RestOfTheFloors(presentFloor, remainingList);
-            }
-        }
-    }
+	/**
+	 * @param currentFloor  points to the current floor
+	 * 
+	 */
+	private void RestOfTheFloors(int currentFloor, List<Integer> remainingList) {
+		for (int i : remainingList) {
+			if (i == currentFloor) {
+				floorsOnSpecificDay.add(i);
+				currentFloor = currentFloor - 1;
+				RestOfTheFloors(currentFloor, remainingList);
+			}
+		}
+	}
 }
